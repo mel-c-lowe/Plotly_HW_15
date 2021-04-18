535080c02f02c4eb21fed73725296f93e0916cee
Melissa Lowe <mel.c.lowe@gmail.com>
2021-04-18 15:13:20 -0500

    Function stubs  established

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,5 +1,21 @@
 console.log("app.js loaded");
 
+// Set up stub functions for each chart
+
+function DrawBarGraph() {
+    console.log("Draw barchart")
+};
+
+function DrawBubbleChart() {
+    console.log("Draw bubble chart")
+};
+
+function ShowMetaData() {
+    console.log("Show metadata")
+};
+
+
+
 
 // Set up default display
 function defaultDisplay() {
@@ -21,7 +37,21 @@ function defaultDisplay() {
                 .text(name)
                 .property("value", name)
         });
+
+        // Set up default sample name
+        var defaultID = sampleNames[0];
+        console.log(defaultID)
     });
+
+    // Set up default bargraph
+    DrawBarGraph();
+
+    // Set up default bubblechart
+    DrawBubbleChart();
+
+    // Set up default metadata
+    ShowMetaData();
+
 };
 
 defaultDisplay();
