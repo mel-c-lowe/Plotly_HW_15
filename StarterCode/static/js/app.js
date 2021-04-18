e45c5c267200e05e56bbcabfca9ec072ede44131
Melissa Lowe <mel.c.lowe@gmail.com>
2021-04-18 14:55:56 -0500

    Dropdown populated

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,28 @@
 console.log("app.js loaded");
 
-// Read in the data
-d3.json("samples.json").then((data) => {
-    console.log(data);
-});
+
+// Set up default display
+function defaultDisplay() {
+    console.log("defaultDisplay")
+    
+    // Populate dropdown with options
+    var selector = d3.select("#selDataset");
+
+    // Read in the data
+    d3.json("samples.json").then(function(data) {
+        console.log(data);
+
+        var sampleNames = data.names;
+
+        console.log(sampleNames)
+
+        sampleNames.forEach(name => {
+            selector.append("option")
+                .text(name)
+                .property("value", name)
+        });
+    });
+};
+
+defaultDisplay();
+
